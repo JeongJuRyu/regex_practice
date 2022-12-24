52e01e3b83c6a678b5b589a7f07cc310f2ba5dbb
JeongJuRyu <totw2018@naver.com>
2022-12-24 13:37:26 +0900

    feat: amend

--- src/main/java/com/study/regex/problem/BOJ1264.java
+++ src/main/java/com/study/regex/problem/BOJ1264.java
@@ -1,2 +1,27 @@
-package com.study.regex.problem;public class Problem1264 {
+package com.study.regex.problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BOJ1264 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        Pattern pattern = Pattern.compile("[aeiouAEIOU]");
+        while(!s.equals("#")){
+            Matcher matcher = pattern.matcher(s);
+            int cnt = 0;
+            while(matcher.find()){
+                cnt++;
+            }
+            System.out.println(cnt);
+            s = br.readLine();
+        }
+    }
 }
